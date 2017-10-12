59cbe63f5118b7ecd14bb17d5cdf51cd4adbf3ed
gosha22008 <gosha22008@yandex.ru>
2017-10-12 18:56:08 +0300

    dmff

--- admin.php
+++ admin.php
@@ -11,8 +11,24 @@
     <input type="submit" value="отправить">
     <input type="reset">
 </form>
+<br>
+<h3>
+    <a href="list.php" style="color: black">К списку тестов</a>
+</h3>
 </body>
 <?php
+
+$parts=array();
+$parts=explode(".", $_FILES['myfile']['name']);
+$js=array("json","");
+if(in_array($parts[count($parts)-1], $js))
+{
+    //все хорошо, обрабатываем файл
+}
+else
+    die("Wrong file type");
+
+
 $json = 'application/json';
 $octetStream = 'application/octet-stream';
 if (isset($_FILES['myfile']) and !empty($_FILES['myfile']['name'])) {
@@ -27,9 +43,6 @@ if (isset($_FILES['myfile']) and !empty($_FILES['myfile']['name'])) {
     }
 }
 ?>
-<br>
-<h3>
-    <a href="list.php" style="color: black">К списку тестов</a>
-</h3>
+
 
 

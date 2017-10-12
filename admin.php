75ed97671fbdc7c3b320f042e20a9971fb22b13b
gosha22008 <gosha22008@yandex.ru>
2017-10-12 19:57:58 +0300

    wldlf

--- admin.php
+++ admin.php
@@ -17,31 +17,45 @@
 </h3>
 </body>
 <?php
-
-$parts=array();
-$parts=explode(".", $_FILES['myfile']['name']);
-$js=array("json","");
-if(in_array($parts[count($parts)-1], $js))
-{
-    //все хорошо, обрабатываем файл
-}
-else
-    die("Wrong file type");
-
-
-$json = 'application/json';
-$octetStream = 'application/octet-stream';
 if (isset($_FILES['myfile']) and !empty($_FILES['myfile']['name'])) {
-    if ($_FILES['myfile']['type'] == $json or $_FILES['myfile']['type'] == $octetStream) {
+    $parts = array();
+    $parts = explode(".", $_FILES['myfile']['name']);
+    $js = array("json", "");
+    if (in_array($parts[count($parts) - 1], $js)) {
         if ($_FILES['myfile']['error'] == 0 and move_uploaded_file($_FILES['myfile']['tmp_name'], __DIR__ . '/DownloadedTests/' . $_FILES['myfile']['name'])) {
-            echo 'Файл загружен';
-        } else {
-            echo 'Ошибка! Файл не загружен !';
+            echo 'Файл успешно загружен';
         }
-    } else {
-        echo 'Неверное расширение! Подходят только .json';
-    }
+    } else
+        die("Wrong file type");
+
 }
+
+
+//
+//$parts=array();
+//$parts=explode(".", $_FILES['myfile']['name']);
+//$js=array("json","");
+//if(in_array($parts[count($parts)-1], $js))
+//{
+//    //все хорошо, обрабатываем файл
+//}
+//else
+//    die("Wrong file type");
+//
+//
+//$json = 'application/json';
+//$octetStream = 'application/octet-stream';
+//if (isset($_FILES['myfile']) and !empty($_FILES['myfile']['name'])) {
+//    if ($_FILES['myfile']['type'] == $json or $_FILES['myfile']['type'] == $octetStream) {
+//        if ($_FILES['myfile']['error'] == 0 and move_uploaded_file($_FILES['myfile']['tmp_name'], __DIR__ . '/DownloadedTests/' . $_FILES['myfile']['name'])) {
+//            echo 'Файл загружен';
+//        } else {
+//            echo 'Ошибка! Файл не загружен !';
+//        }
+//    } else {
+//        echo 'Неверное расширение! Подходят только .json';
+//    }
+//}
 ?>
 
 

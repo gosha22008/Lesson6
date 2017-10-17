21746f54209ab3d2c8b9bb0bdd13bebc95939fc8
gosha22008 <gosha22008@yandex.ru>
2017-10-17 19:55:51 +0300

    kfkfkf

--- test.php
+++ test.php
@@ -7,7 +7,7 @@
 <?php
 $aa = $_GET;
 $k = key($aa);
-$file = file_get_contents(__DIR__.'/DownloadedTests/'. $_GET[$k]);
+$file = file_get_contents(__DIR__ . '/DownloadedTests/' . $_GET[$k]);
 $file1 = json_decode($file, true);
 ?>
 <body>
@@ -16,14 +16,24 @@ $file1 = json_decode($file, true);
     <a href="list.php" style="color: black">К списку тестов</a>
 </h3>
 <form action="" method="POST">
-    <?php foreach ($file1 as $k => $v) { ?>
-        <fieldset>
-            <legend><?= "$k." . $file1[$k]["q$k"] ?></legend>
-            <label><input name="q<?= $k ?>" value="0" type="radio"> <?= $file1[$k]['answer']['a0'] ?></label>
-            <label><input name="q<?= $k ?>" value="1" type="radio"> <?= $file1[$k]['answer']['a1'] ?></label>
-            <label><input name="q<?= $k ?>" value="2" type="radio"> <?= $file1[$k]['answer']['a2'] ?></label>
-        </fieldset>
-    <?php } ?>
+    <?php
+    $i = 0;
+    foreach ($file1 as $k => $v) {
+        if (isset($file1[$k]["q$k"]) and isset($file1[$k]['answer']["a$k"])) { ?>
+            <fieldset>
+                <legend><?= "$k." . $file1[$k]["q$k"] ?></legend>
+                <label><input name="q<?= $k ?>" value="0" type="radio"> <?= $file1[$k]['answer']['a0'] ?></label>
+                <label><input name="q<?= $k ?>" value="1" type="radio"> <?= $file1[$k]['answer']['a1'] ?></label>
+                <label><input name="q<?= $k ?>" value="2" type="radio"> <?= $file1[$k]['answer']['a2'] ?></label>
+            </fieldset>
+            <?php
+        } else {
+            $i++;
+            if ($i == 1) {
+                echo 'неверный формат теста!';
+            }
+        }
+    } ?>
     <input value="Отправить" type="submit">
 </form>
 </body>
